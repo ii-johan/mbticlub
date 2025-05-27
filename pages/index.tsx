fcc1e015afe573a226e282b5d27177c5738add9a
Your Name <dikaris@naver.com>
2025-05-28 06:19:30 +0900

    feat:updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -132,7 +132,7 @@ const Home: React.FC = () => {
               e.currentTarget.style.transform = 'scale(1.03)'; // 클릭 후 원복 (호버 상태로)
             }}
           >
-            Test Go (80)
+            Slow Test(80)
           </button>
 
           {/* Full Test 버튼 */}
@@ -168,7 +168,7 @@ const Home: React.FC = () => {
               e.currentTarget.style.transform = 'scale(1.03)';
             }}
           >
-            Test Go (80)
+            Slow Test(80)
           </button>
         </div>
       </main>
